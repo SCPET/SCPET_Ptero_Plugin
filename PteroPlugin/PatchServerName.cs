0a9306b08045eff0626830772fbe97dde5237a56
VirtualBrightPlayz <lego.trains.b@gmail.com>
2021-01-03 22:17:42 -0700

    Initial commit

---
+++ PteroPlugin/PatchServerName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using VirtualBrightPlayz.SCP_ET.Misc;
+
+namespace ExamplePlugin
+{
+    public class PatchServerName
+    {
+        [HarmonyPatch(typeof(GameSettings), nameof(GameSettings.LoadServerSettings))]
+        public static void Postfix()
+        {
+            GameSettings.serverSettings.serverName = GameSettings.serverSettings.serverName.Replace("\\u003c", "<").Replace("\\u003e", ">");
+        }
+    }
+}
